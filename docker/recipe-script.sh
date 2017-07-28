2603ef1f998ec632503d54f7b1e4f0d0a859ed07
ubuntu <wkhunt3@gmail.com>
2017-07-28 18:51:04 -0400

    Channel defaults

--- docker/recipe-script.sh
+++ docker/recipe-script.sh
@@ -1,5 +1,10 @@
 export PATH="$HOME/miniconda2/bin:$PATH"
 rm -rf ~/miniconda2/conda-bld
+conda config --add channels local
+conda config --add channels https://proxy:proxy@nexus.devops.geointservices.io/content/repositories/beachfront-conda
+conda config --add channels conda-forge
+conda config --add channels bioconda
+conda config --add channels default
 cd share/recipes
 vendoredFolders=$(ls)
 for f in $vendoredFolders; do
