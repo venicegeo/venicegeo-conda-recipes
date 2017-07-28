79b843d018e5a1410bdcc706146486c338b74dd4
ubuntu <wkhunt3@gmail.com>
2017-07-28 18:54:53 -0400

    debug

--- docker/recipe-script.sh
+++ docker/recipe-script.sh
@@ -1,13 +1,25 @@
 export PATH="$HOME/miniconda2/bin:$PATH"
+echo Clearing out conda-bld
 rm -rf ~/miniconda2/conda-bld
+echo Rebuilding conda-bld
+mkdir -p ~/miniconda2/conda-bld/linux-64
+mkdir -p ~/miniconda2/conda-bld/noarch
+conda index ~/miniconda2/conda-bld/linux-64
+conda index ~/miniconda2/conda-bld/noarch
+echo Adding local
 conda config --add channels local
+echo Adding nexus
 conda config --add channels https://proxy:proxy@nexus.devops.geointservices.io/content/repositories/beachfront-conda
+echo Adding conda-forge
 conda config --add channels conda-forge
+echo Adding bioconda
 conda config --add channels bioconda
+echo Adding defaults
 conda config --add channels default
 cd share/recipes
 vendoredFolders=$(ls)
 for f in $vendoredFolders; do
+  echo $f
   conda build $f
 done
 ls ~/miniconda2/conda-bld/linux-64
