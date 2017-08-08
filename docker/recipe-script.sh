7aeba68653e27dfa22024a6bec1e4de08defbc43
ubuntu <wkhunt3@gmail.com>
2017-08-08 11:38:32 -0400

    Adding quite tag to wget

--- docker/recipe-script.sh
+++ docker/recipe-script.sh
@@ -26,7 +26,7 @@ for f in $toKeep; do
   echo $f
 done
 mkdir linux-64 && cd linux-64
-wget -r -l1 -e robots=off -nH -nd --reject="index.html*" --no-parent --no-cookies https://nexus.devops.geointservices.io/content/repositories/beachfront-conda/linux-64/ --user=proxy --password=proxy
+wget -q -r -l1 -e robots=off -nH -nd --reject="index.html*" --no-parent --no-cookies https://nexus.devops.geointservices.io/content/repositories/beachfront-conda/linux-64/ --user=proxy --password=proxy
 mv ~/miniconda2/conda-bld/linux-64/* .
 conda index .
 deleteString="find . -type f"
