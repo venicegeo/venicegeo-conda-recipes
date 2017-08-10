685a4b778ac525588c88f9b11c120d4f72fe3d13
wkhunt <wkhunt3@gmail.com>
2017-08-10 11:48:55 -0400

    Update recipe-script.sh

--- docker/recipe-script.sh
+++ docker/recipe-script.sh
@@ -6,12 +6,12 @@ mkdir -p ~/miniconda2/conda-bld/linux-64
 mkdir -p ~/miniconda2/conda-bld/noarch
 conda index ~/miniconda2/conda-bld/linux-64
 conda index ~/miniconda2/conda-bld/noarch
-#echo Adding bioconda
-#conda config --add channels bioconda
-#echo Adding conda-forge
-#conda config --add channels conda-forge
-echo Adding nexus
-conda config --add channels https://proxy:proxy@nexus.devops.geointservices.io/content/repositories/beachfront-conda
+echo Adding bioconda
+conda config --add channels bioconda
+echo Adding conda-forge
+conda config --add channels conda-forge
+#echo Adding nexus
+#conda config --add channels https://proxy:proxy@nexus.devops.geointservices.io/content/repositories/beachfront-conda
 echo Adding local
 conda config --add channels local
 cd share/recipes
