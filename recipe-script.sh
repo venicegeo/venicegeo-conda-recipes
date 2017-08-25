d0efe0f1036c0a2b4bdf01f76652c2eab2ec3f7d
wkhunt <wkhunt3@gmail.com>
2017-08-25 16:34:28 -0400

    Update recipe-script.sh

--- recipe-script.sh
+++ recipe-script.sh
@@ -1,15 +1,25 @@
+#!/bin/bash
+
+user=$1
+pass=$2
+channel_url=$3
+
+creds_channel_url=https://$user:$pass@$channel_url
+channel_url=https://$channel_url
+
+rm -f $HOME/.condarc
 echo Clearing out conda-bld
-rm -rf SED_LOC/miniconda2/conda-bld
+rm -rf $HOME/miniconda2/conda-bld
 echo Removing defaults
 conda config --remove channels defaults
 echo Adding nexus
-conda config --add channels SED_CREDS_CHANNEL
+conda config --add channels $creds_channel_url
 conda install conda-build -y -q
 echo Rebuilding conda-bld
-mkdir -p SED_LOC/miniconda2/conda-bld/linux-64
-mkdir -p SED_LOC/miniconda2/conda-bld/noarch
-conda index SED_LOC/miniconda2/conda-bld/linux-64
-conda index SED_LOC/miniconda2/conda-bld/noarch
+mkdir -p $HOME/miniconda2/conda-bld/linux-64
+mkdir -p $HOME/miniconda2/conda-bld/noarch
+conda index $HOME/miniconda2/conda-bld/linux-64
+conda index $HOME/miniconda2/conda-bld/noarch
 echo Adding local
 conda config --add channels local
 cd recipes
@@ -19,17 +29,18 @@ for f in $vendoredFolders; do
   conda build $f --old-build-string -q
 done
 cd ..
-toKeep=$(ls SED_LOC/miniconda2/conda-bld/linux-64)
+toKeep=$(ls $HOME/miniconda2/conda-bld/linux-64)
 for f in $toKeep; do
   echo $f
 done
 mkdir linux-64 && cd linux-64
-wget -q -r -l1 -e robots=off -nH -nd --reject="index.html*" --no-parent --no-cookies SED_CHANNEL/linux-64/ --user=SED_USER --password=SED_PASS
-mv SED_LOC/miniconda2/conda-bld/linux-64/* .
+wget -q -r -l1 -e robots=off -nH -nd --reject="index.html*" --no-parent --no-cookies $channel_url/linux-64/ --user=$user --password=$pass
+mv $HOME/miniconda2/conda-bld/linux-64/* .
 conda index .
 deleteString="find . -type f"
 for f in $toKeep; do
   deleteString=$deleteString" ! -name $f"
 done
 deleteString=$deleteString" -delete"
+echo $deleteString
 $deleteString
