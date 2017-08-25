bcd96f5c454200c1c3aa9fb1d4a213da49d6aefc
wkhunt <wkhunt3@gmail.com>
2017-08-24 21:20:55 -0400

    Update build.sh

--- recipes/pzsvc-exec/build.sh
+++ recipes/pzsvc-exec/build.sh
@@ -2,7 +2,6 @@
 
 export GOPATH=$PWD
 export GOBIN=$GOPATH/bin
-export GOROOT=/usr/local/go
 export PATH=$GOROOT/bin:$PATH
 mkdir -p src/github.com/venicegeo/pzsvc-exec
 mv `ls | grep -v src` src/github.com/venicegeo/pzsvc-exec/ && mkdir bin
