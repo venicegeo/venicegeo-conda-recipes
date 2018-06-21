4432034aa3128e41fe9705d397cfd92ddbf01f39
wkhunt <wkhunt3@gmail.com>
2018-06-21 13:50:35 -0400

    Use branch as version

--- tag.sh
+++ tag.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-tag=$1
-if [ -z "$tag" ]; then
+v=$1
+if [ -z "$v" ]; then
   exit 1
 fi
 cd recipes
 for f in $(ls); do
   cd "$f"
   pwd
-  tmp=$(cat checkout.txt)
-  echo $tmp" "$tag > checkout.txt
-  i=0
-  sed -i "s/beachfront x/beachfront $tag/g" meta.yaml
-  sed -i "s/version: x/version: $tag/g" meta.yaml
+  sed -i "s/REPLACE_VERSION/$v/g" meta.yaml
+  sed -i "s/REPLACE_VERSION/$v/g" checkout.txt
   cd ..
 done
